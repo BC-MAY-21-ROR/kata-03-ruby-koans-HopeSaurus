d56efe979eb54d82d0cae6515d125a9750594f6b
HopeSaurus <xavi0@hotmail.com>
2022-08-25 12:06:59 -0500

    80% koans completion

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -6,6 +6,23 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_reader :values
+
+  def initialize()
+    @values = []
+  end
+
+  def roll(n_throws)
+    @values = []
+    n_throws.times do |dice|
+      @values << rand(1..6)
+    end
+  end
+  
+end
+
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
